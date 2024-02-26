d40151c9f5528381a34f8522c01e217a014392d4
g-sillva <silvagabriel.limars@gmail.com>
2024-02-26 13:13:05 -0300

    Interação ao clicar nos favoritos e notificação no componente card alocacao

--- client/src/components/CardAlocacao.js
+++ client/src/components/CardAlocacao.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 const CardAlocacao = ({ responsavel, disciplina, local, tipo }) => {
+  const [isFavorito, setIsFavorito] = useState(false);
+  const [isNotificacao, setIsNotificacao] = useState(false);
+
+  const handleFavorito = () => {
+    setIsFavorito(!isFavorito);
+  };
+
+  const handleNotificacao = () => {
+    setIsNotificacao(!isNotificacao);
+  };
+
+  const handleOpcoes = () => {};
+
   return (
     <View style={styles.container}>
       <View>
@@ -18,12 +31,41 @@ const CardAlocacao = ({ responsavel, disciplina, local, tipo }) => {
         <View style={styles.contentHeader}>
           <Text style={styles.responsibleText}>{responsavel}</Text>
           <View style={styles.iconsContainer}>
-            <Ionicons name="star-outline" size={16} color="#6B7280" />
-            <Ionicons name="notifications-outline" size={16} color="#6B7280" />
+            {isFavorito ? (
+              <Ionicons
+                name="star"
+                size={16}
+                color="#F4E328"
+                onPress={handleFavorito}
+              />
+            ) : (
+              <Ionicons
+                name="star-outline"
+                size={16}
+                color="#6B7280"
+                onPress={handleFavorito}
+              />
+            )}
+            {isNotificacao ? (
+              <Ionicons
+                name="notifications"
+                size={16}
+                color="#FA521D"
+                onPress={handleNotificacao}
+              />
+            ) : (
+              <Ionicons
+                name="notifications-outline"
+                size={16}
+                color="#6B7280"
+                onPress={handleNotificacao}
+              />
+            )}
             <Ionicons
               name="ellipsis-vertical-outline"
               size={16}
               color="#6B7280"
+              onPress={handleOpcoes}
             />
           </View>
         </View>
