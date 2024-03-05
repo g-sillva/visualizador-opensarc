aa7bfd08e7749dd7d8cf30a182b2c5e78a661ac3
g-sillva <silvagabriel.limars@gmail.com>
2024-03-05 05:21:32 -0300

    Added redis cache & schedule to fetch resources

---
+++ server/api/cache.py
@@ -0,0 +1,18 @@
+import redis
+import json
+from datetime import datetime
+
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+def add_to_cache(key, value):
+    data_to_store = {'value': value, 'timestamp': datetime.now().isoformat()}
+    serialized_data = json.dumps(data_to_store)
+    r.set(key, serialized_data)
+
+def get_from_cache(key):
+    serialized_data = r.get(key)
+    if serialized_data:
+        data = json.loads(serialized_data)
+        return data
+    else:
+        return None
--- server/api/main.py
+++ server/api/main.py
@@ -2,11 +2,11 @@ import os
 import atexit
 import requests
 from dotenv import load_dotenv
-
-#from apscheduler.schedulers.background import BackgroundScheduler
-
+from cache import add_to_cache, get_from_cache
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 from flask_restful import Resource, Api
+from datetime import datetime, timedelta
 
 load_dotenv()
 app = Flask(__name__)
@@ -14,18 +14,27 @@ api = Api(app)
 
 class Resources(Resource):
     def get(self):
-        r = requests.get(os.getenv('SCRAPY_SERVER_URL')).json()
-        items = r['items']
-        return items
+        cached_resource = get_from_cache('resources')
+
+        if cached_resource is None:
+            fetch_resources()
+
+        return get_from_cache('resources')
 
 api.add_resource(Resources, '/resources')
 
-# scheduler = BackgroundScheduler()
-# scheduler.add_job(func=job, trigger="interval", seconds=10)
-# scheduler.start()
+def fetch_resources():
+    print('Fetching resources...')
+    r = requests.get(os.getenv('SCRAPY_SERVER_URL')).json()
+    items = r['items']
+    add_to_cache('resources', items)
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=fetch_resources, trigger="interval", minutes=5)
+scheduler.start()
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 # Shut down the scheduler when exiting the app
-# atexit.register(lambda: scheduler.shutdown())
+atexit.register(lambda: scheduler.shutdown())
