c65efa1ead79101725609b381e505d28e4a92cf9
g-sillva <silvagabriel.limars@gmail.com>
2024-07-08 15:08:59 -0300

    added header and remaining filters

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -4,56 +4,46 @@ import React, { cache, useEffect, useState } from "react";
 import styles from "./page.module.css";
 import { Item } from "../_models/Item";
 import Card from "../_components/card/Card";
-import { convertTimeCode } from "../_utils/converter";
+import { convertResourceTypeText, convertTimeCode } from "../_utils/converter";
 import InputText from "../_components/input-text/InputText";
 import SelectOne from "../_components/select-one/SelectOne";
 
 export default function Home() {
   const [data, setData] = useState<any>();
-  const [filters, setFilters] = useState({ general: "", subject: "Todas" });
+  const [filters, setFilters] = useState({
+    general: "",
+    subject: "Todas",
+    responsible: "Todos",
+    type: "Todos",
+    resource: "Todos",
+    status: "Todos",
+  });
   const [filteredData, setFilteredData] = useState<any>();
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<null | string>(null);
 
   const handleGeneralFilter = (value: string) => {
     setFilters({ ...filters, general: value });
-
-    const filteredData = Object.keys(data).reduce((acc: any, key) => {
-      const items = data[key] as Item[];
-      const filteredItems = items.filter((item) => {
-        const values = Object.values(item).join(" ").toLowerCase();
-        return values.includes(value.toLowerCase());
-      });
-
-      if (filteredItems.length) {
-        acc[key] = filteredItems;
-      }
-
-      return acc;
-    }, {});
-
-    setFilteredData(filteredData);
   };
 
   const handleSubjectFilter = (value: string) => {
     setFilters({ ...filters, subject: value });
+  };
 
-    if (value === "Todas") return setFilteredData(data);
+  const handleResponsibleFilter = (value: string) => {
+    setFilters({ ...filters, responsible: value });
+  };
 
-    const filteredData = Object.keys(data).reduce((acc: any, key) => {
-      const items = data[key] as Item[];
-      const filteredItems = items.filter((item) => {
-        return item.subject.toLowerCase().includes(value.toLowerCase());
-      });
+  const handleTypeFilter = (value: string) => {
+    setFilters({ ...filters, type: value });
+  };
 
-      if (filteredItems.length) {
-        acc[key] = filteredItems;
-      }
+  const handleResourceFilter = (value: string) => {
+    setFilters({ ...filters, resource: value });
+  };
 
-      return acc;
-    }, {});
-
-    setFilteredData(filteredData);
+  const handleStatusFilter = (value: string) => {
+    setFilters({ ...filters, status: value });
   };
 
   const getSubjectOptions = () => {
@@ -68,6 +58,62 @@ export default function Home() {
       return acc;
     }, []);
     return ["Todas", ...subjects];
+  };
+
+  const getResponsibleOptions = () => {
+    const responsibles = Object.keys(data).reduce((acc: string[], key) => {
+      const items = data[key] as Item[];
+      items.forEach((item) => {
+        if (!acc.includes(item.responsible)) {
+          acc.push(item.responsible);
+        }
+      });
+
+      return acc;
+    }, []);
+    return ["Todos", ...responsibles];
+  };
+
+  const getTypeOptions = () => {
+    const types = Object.keys(data).reduce((acc: string[], key) => {
+      const items = data[key] as Item[];
+      items.forEach((item) => {
+        if (!acc.includes(convertResourceTypeText(item.type))) {
+          acc.push(convertResourceTypeText(item.type));
+        }
+      });
+
+      return acc;
+    }, []);
+    return ["Todos", ...types];
+  };
+
+  const getResourceOptions = () => {
+    const resources = Object.keys(data).reduce((acc: string[], key) => {
+      const items = data[key] as Item[];
+      items.forEach((item) => {
+        if (!acc.includes(item.resource)) {
+          acc.push(item.resource);
+        }
+      });
+
+      return acc;
+    }, []);
+    return ["Todos", ...resources];
+  };
+
+  const getStatusOptions = () => {
+    const statuses = Object.keys(data).reduce((acc: string[], key) => {
+      const items = data[key] as Item[];
+      items.forEach((item) => {
+        if (!acc.includes(item.status)) {
+          acc.push(item.status);
+        }
+      });
+
+      return acc;
+    }, []);
+    return ["Todos", ...statuses];
   };
 
   useEffect(() => {
@@ -91,8 +137,61 @@ export default function Home() {
     });
   }, []);
 
+  useEffect(() => {
+    if (!data) return;
+
+    const filteredData = Object.keys(data).reduce((acc: any, key) => {
+      const items = data[key] as Item[];
+      const filteredItems = items.filter((item) => {
+        if (filters.subject !== "Todas" && item.subject !== filters.subject) {
+          return false;
+        }
+
+        if (
+          filters.responsible !== "Todos" &&
+          item.responsible !== filters.responsible
+        ) {
+          return false;
+        }
+
+        if (
+          filters.type !== "Todos" &&
+          convertResourceTypeText(item.type) !== filters.type
+        ) {
+          return false;
+        }
+
+        if (
+          filters.resource !== "Todos" &&
+          item.resource !== filters.resource
+        ) {
+          return false;
+        }
+
+        if (filters.status !== "Todos" && item.status !== filters.status) {
+          return false;
+        }
+
+        const values = Object.values(item).join(" ").toLowerCase();
+        return values.includes(filters.general.toLowerCase());
+      });
+
+      if (filteredItems.length) {
+        acc[key] = filteredItems;
+      }
+
+      return acc;
+    }, {});
+
+    setFilteredData(filteredData);
+  }, [filters]);
+
   return (
     <main className={styles.main}>
+      <div className={styles.header}>
+        <h1>Visualizador OpenSarc</h1>
+        <p>Visualização melhorada dos recursos alocados no OpenSarc.</p>
+      </div>
       {isLoading ? (
         <div>Carregando...</div>
       ) : error ? (
@@ -106,9 +205,33 @@ export default function Home() {
               options={getSubjectOptions()}
               onSelect={handleSubjectFilter}
             />
+            <SelectOne
+              label="Responsável"
+              selectedOption={filters.responsible}
+              options={getResponsibleOptions()}
+              onSelect={handleResponsibleFilter}
+            />
+            <SelectOne
+              label="Tipo"
+              selectedOption={filters.type}
+              options={getTypeOptions()}
+              onSelect={handleTypeFilter}
+            />
+            <SelectOne
+              label="Recurso"
+              selectedOption={filters.resource}
+              options={getResourceOptions()}
+              onSelect={handleResourceFilter}
+            />
+            <SelectOne
+              label="Status"
+              selectedOption={filters.status}
+              options={getStatusOptions()}
+              onSelect={handleStatusFilter}
+            />
             <InputText
+              label="Geral"
               placeholder="Buscar..."
-              label="Filtrar"
               value={filters.general}
               onChange={handleGeneralFilter}
             />
